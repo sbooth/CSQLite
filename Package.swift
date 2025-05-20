d6095a31940981a04fa528c1df88b90fac0c1701
Stephen F. Booth <me@sbooth.org>
2025-05-20 11:01:22 -0500

    Swap Package files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /*
@@ -34,17 +34,17 @@ let compileTimeOptions: [CSetting] = [
 	// https://sqlite.org/limits.html#max_expr_depth
 	.define("SQLITE_MAX_EXPR_DEPTH", to: "0"),
 	// https://sqlite.org/compile.html#omit_decltype
-	.define("SQLITE_OMIT_DECLTYPE", .when(traits: ["OMIT_DECLTYPE"])),
+	.define("SQLITE_OMIT_DECLTYPE"),
 	// https://sqlite.org/compile.html#omit_deprecated
-	.define("SQLITE_OMIT_DEPRECATED", .when(traits: ["OMIT_DEPRECATED"])),
+	.define("SQLITE_OMIT_DEPRECATED"),
 	// https://sqlite.org/compile.html#omit_progress_callback
-	.define("SQLITE_OMIT_PROGRESS_CALLBACK", .when(traits: ["OMIT_PROGRESS_CALLBACK"])),
+	.define("SQLITE_OMIT_PROGRESS_CALLBACK"),
 	// https://sqlite.org/compile.html#omit_shared_cache
-	.define("SQLITE_OMIT_SHARED_CACHE", .when(traits: ["OMIT_SHARED_CACHE"])),
+	.define("SQLITE_OMIT_SHARED_CACHE"),
 	// https://sqlite.org/compile.html#use_alloca
-	.define("SQLITE_USE_ALLOCA", .when(traits: ["USE_ALLOCA"])),
+	.define("SQLITE_USE_ALLOCA"),
 	// https://sqlite.org/compile.html#omit_autoinit
-	.define("SQLITE_OMIT_AUTOINIT", .when(traits: ["OMIT_AUTOINIT"])),
+	.define("SQLITE_OMIT_AUTOINIT"),
 	// https://sqlite.org/compile.html#strict_subtype
 	.define("SQLITE_STRICT_SUBTYPE", to: "1"),
 ]
@@ -59,35 +59,29 @@ let platformConfiguration: [CSetting] = [
 /// Features
 /// - seealso: [Options To Enable Features Normally Turned Off](https://sqlite.org/compile.html#_options_to_enable_features_normally_turned_off)
 let features: [CSetting] = [
-	// https://sqlite.org/bytecodevtab.html
-	.define("SQLITE_ENABLE_BYTECODE_VTAB", .when(traits: ["ENABLE_BYTECODE_VTAB"])),
 	// https://sqlite.org/c3ref/column_database_name.html
-	.define("SQLITE_ENABLE_COLUMN_METADATA", .when(traits: ["ENABLE_COLUMN_METADATA"])),
-	// https://sqlite.org/dbpage.html
-	.define("SQLITE_ENABLE_DBPAGE_VTAB", .when(traits: ["ENABLE_DBPAGE_VTAB"])),
-	// https://sqlite.org/dbstat.html
-	.define("SQLITE_ENABLE_DBSTAT_VTAB", .when(traits: ["ENABLE_DBSTAT_VTAB"])),
+//	.define("SQLITE_ENABLE_COLUMN_METADATA"),
 	// https://sqlite.org/fts5.html
-	.define("SQLITE_ENABLE_FTS5", .when(traits: ["ENABLE_FTS5"])),
+	.define("SQLITE_ENABLE_FTS5"),
 	// https://sqlite.org/geopoly.html
-	.define("SQLITE_ENABLE_GEOPOLY", .when(traits: ["ENABLE_GEOPOLY"])),
-//	.define("SQLITE_ENABLE_ICU", .when(traits: ["ENABLE_ICU"])),
+//	.define("SQLITE_ENABLE_GEOPOLY"),
+//	.define("SQLITE_ENABLE_ICU"),
 	// https://sqlite.org/lang_mathfunc.html
-	.define("SQLITE_ENABLE_MATH_FUNCTIONS", .when(traits: ["ENABLE_MATH_FUNCTIONS"])),
-	// https://sqlite.org/c3ref/expanded_sql.html
-	.define("SQLITE_ENABLE_NORMALIZE", .when(traits: ["ENABLE_NORMALIZE"])),
+	.define("SQLITE_ENABLE_MATH_FUNCTIONS"),
+	// --> For pre-update hook support uncomment the following define
 	// https://sqlite.org/c3ref/preupdate_blobwrite.html
-	.define("SQLITE_ENABLE_PREUPDATE_HOOK", .when(traits: ["ENABLE_PREUPDATE_HOOK"])),
+//	.define("SQLITE_ENABLE_PREUPDATE_HOOK"),
 	// https://sqlite.org/rtree.html
-	.define("SQLITE_ENABLE_RTREE", .when(traits: ["ENABLE_RTREE"])),
+	.define("SQLITE_ENABLE_RTREE"),
+	// --> For session support uncomment the following define and enable the pre-update hook
 	// https://sqlite.org/sessionintro.html
-	.define("SQLITE_ENABLE_SESSION", .when(traits: ["ENABLE_SESSION"])),
+//	.define("SQLITE_ENABLE_SESSION"),
 	// https://sqlite.org/c3ref/snapshot.html
-	.define("SQLITE_ENABLE_SNAPSHOT", .when(traits: ["ENABLE_SNAPSHOT"])),
+	.define("SQLITE_ENABLE_SNAPSHOT"),
 	// https://sqlite.org/stmt.html
-	.define("SQLITE_ENABLE_STMTVTAB", .when(traits: ["ENABLE_STMTVTAB"])),
+	.define("SQLITE_ENABLE_STMTVTAB"),
 	// https://sqlite.org/fileformat2.html#stat4tab
-	.define("SQLITE_ENABLE_STAT4", .when(traits: ["ENABLE_STAT4"])),
+	.define("SQLITE_ENABLE_STAT4"),
 ]
 
 let package = Package(
@@ -99,108 +93,6 @@ let package = Package(
 			targets: [
 				"CSQLite",
 			]),
-	],
-	traits: [
-		// Compile-time options
-		.trait(
-			name: "OMIT_DECLTYPE",
-			description: "Omit the ability to return the declared type of columns"
-		),
-		.trait(
-			name: "OMIT_DEPRECATED",
-			description: "Omit deprecated interfaces and features"
-		),
-		.trait(
-			name: "OMIT_PROGRESS_CALLBACK",
-			description: "Omit progress callback"
-		),
-		.trait(
-			name: "OMIT_SHARED_CACHE",
-			description: "Omit shared cache support"
-		),
-		.trait(
-			name: "USE_ALLOCA",
-			description: "Use alloca to dynamically allocate temporary stack space"
-		),
-		.trait(
-			name: "OMIT_AUTOINIT",
-			description: "Omit automatic library initialization"
-		),
-		// Features
-		.trait(
-			name: "ENABLE_BYTECODE_VTAB",
-			description: "Enables bytecode and tables_used table-valued functions"
-		),
-		.trait(
-			name: "ENABLE_COLUMN_METADATA",
-			description: "Enables column and table metadata functions"
-		),
-		.trait(
-			name: "ENABLE_DBPAGE_VTAB",
-			description: "Enables the sqlite_dbpage virtual table"
-		),
-		.trait(
-			name: "ENABLE_DBSTAT_VTAB",
-			description: "Enables the dbstat virtual table"
-		),
-		.trait(
-			name: "ENABLE_FTS5",
-			description: "Enables version 5 of the full-text search engine (fts5)"
-		),
-		.trait(
-			name: "ENABLE_GEOPOLY",
-			description: "Enables the Geopoly extension"
-		),
-		.trait(
-			name: "ENABLE_MATH_FUNCTIONS",
-			description: "Enables the built-in SQL math functions"
-		),
-		.trait(
-			name: "ENABLE_NORMALIZE",
-			description: "Enables the sqlite3_normalized_sql function"
-		),
-		.trait(
-			name: "ENABLE_PREUPDATE_HOOK",
-			description: "Enables the pre-update hook"
-		),
-		.trait(
-			name: "ENABLE_RTREE",
-			description: "Enables the R*Tree index extension"
-		),
-		.trait(
-			name: "ENABLE_SESSION",
-			description: "Enables the pre-update hook and session extension",
-			enabledTraits: [
-				"ENABLE_PREUPDATE_HOOK",
-			]
-		),
-		.trait(
-			name: "ENABLE_SNAPSHOT",
-			description: "Enables support for database snapshots"
-		),
-		.trait(
-			name: "ENABLE_STMTVTAB",
-			description: "Enables the sqlite_stmt virtual table"
-		),
-		.trait(
-			name: "ENABLE_STAT4",
-			description: "Enables the sqlite_stat4 table"
-		),
-		// Default traits
-		.default(enabledTraits: [
-			"OMIT_DECLTYPE",
-			"OMIT_DEPRECATED",
-			"OMIT_PROGRESS_CALLBACK",
-			"OMIT_SHARED_CACHE",
-			"USE_ALLOCA",
-			"OMIT_AUTOINIT",
-			"ENABLE_FTS5",
-			"ENABLE_MATH_FUNCTIONS",
-			"ENABLE_RTREE",
-			"ENABLE_SNAPSHOT",
-			"ENABLE_STMTVTAB",
-			"ENABLE_STAT4",
-		]),
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
