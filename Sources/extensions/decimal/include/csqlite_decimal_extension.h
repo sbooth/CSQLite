96205313666dc7e9e1967da6904c1618a685d4ca
Stephen F. Booth <me@sbooth.org>
2025-05-30 10:53:06 -0500

    Fix copy/paste error

--- Sources/extensions/decimal/include/csqlite_decimal_extension.h
+++ Sources/extensions/decimal/include/csqlite_decimal_extension.h
@@ -16,37 +16,7 @@
 // MARK: - Database extensions
 // See https://sqlite.org/loadext.html
 
-/// Equivalent to `sqlite3_auto_extension(sqlite3_carray_init)`
-int csqlite_sqlite3_auto_extension_carray(void);
-/// Equivalent to `sqlite3_cancel_auto_extension(sqlite3_carray_init)`
-int csqlite_sqlite3_cancel_auto_extension_carray(void);
-
 /// Equivalent to `sqlite3_auto_extension(sqlite3_decimal_init)`
 int csqlite_sqlite3_auto_extension_decimal(void);
 /// Equivalent to `sqlite3_cancel_auto_extension(sqlite3_decimal_init)`
 int csqlite_sqlite3_cancel_auto_extension_decimal(void);
-
-/// Equivalent to `sqlite3_auto_extension(sqlite3_ieee_init)`
-int csqlite_sqlite3_auto_extension_ieee754(void);
-/// Equivalent to `sqlite3_cancel_auto_extension(sqlite3_ieee_init)`
-int csqlite_sqlite3_cancel_auto_extension_ieee754(void);
-
-/// Equivalent to `sqlite3_auto_extension(sqlite3_percentile_init)`
-int csqlite_sqlite3_auto_extension_percentile(void);
-/// Equivalent to `sqlite3_cancel_auto_extension(sqlite3_percentile_init)`
-int csqlite_sqlite3_cancel_auto_extension_percentile(void);
-
-/// Equivalent to `sqlite3_auto_extension(sqlite3_series_init)`
-int csqlite_sqlite3_auto_extension_series(void);
-/// Equivalent to `sqlite3_cancel_auto_extension(sqlite3_series_init)`
-int csqlite_sqlite3_cancel_auto_extension_series(void);
-
-/// Equivalent to `sqlite3_auto_extension(sqlite3_shathree_init)`
-int csqlite_sqlite3_auto_extension_sha3(void);
-/// Equivalent to `sqlite3_cancel_auto_extension(sqlite3_shathree_init)`
-int csqlite_sqlite3_cancel_auto_extension_sha3(void);
-
-/// Equivalent to `sqlite3_auto_extension(sqlite3_uuid_init)`
-int csqlite_sqlite3_auto_extension_uuid(void);
-/// Equivalent to `sqlite3_cancel_auto_extension(sqlite3_uuid_init)`
-int csqlite_sqlite3_cancel_auto_extension_uuid(void);
